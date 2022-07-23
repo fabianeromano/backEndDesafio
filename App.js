c0d14b295362ab7dc654beb26b3a816f03253b38
fabianeromano <fabian.ed.romano@gmail.com>
2022-07-22 21:58:15 -0300

    Agregando repositorio

---
+++ App.js
@@ -0,0 +1,36 @@
+class Usuario{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = [];
+        this.mascotas = [];
+    }
+    getFullName(){
+        console.log(`${this.nombre} ${this.apellido}`);
+    }
+    addMascota(mascota){
+        this.mascotas.push(mascota);
+        
+    }
+    countMascotas(){
+    console.log(`Cantidad de mascotas: ${this.mascotas.length}`);
+    }
+    addBook(nombre, autor){
+        this.libros.push({nombre, autor});
+    }
+    getBookNames(){
+        console.log(this.libros.map(el=>el.nombre));
+    }
+
+}
+
+
+let usuario = new Usuario("Fabian", "Romano");
+usuario.getFullName();
+usuario.addMascota("Perro");
+usuario.addMascota("Gato");
+usuario.countMascotas();
+usuario.addBook("El señor de las moscas", "William Golding");
+usuario.addBook("Fundacion", "Isaac Asimov");
+usuario.getBookNames();
+console.log(usuario);
