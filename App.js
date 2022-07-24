332f37e3456a3ef5623c55749e35583e25c5d214
fabianeromano <fabian.ed.romano@gmail.com>
2022-07-24 20:42:17 -0300

    Agregando correcciones al desafio1

--- App.js
+++ App.js
@@ -1,36 +1,41 @@
-class Usuario{
-    constructor(nombre, apellido){
+class Usuario {
+    constructor(nombre, apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.libros = [];
         this.mascotas = [];
     }
-    getFullName(){
-        console.log(`${this.nombre} ${this.apellido}`);
+    getFullName() {
+        return `${this.nombre} ${this.apellido}`;
     }
-    addMascota(mascota){
+    addMascota(mascota) {
         this.mascotas.push(mascota);
-        
+
     }
-    countMascotas(){
-    console.log(`Cantidad de mascotas: ${this.mascotas.length}`);
+    countMascotas() {
+        return `Cantidad de mascotas: ${this.mascotas.length}`;
     }
-    addBook(nombre, autor){
-        this.libros.push({nombre, autor});
+    addBook(nombre, autor) {
+        this.libros.push({
+            nombre,
+            autor
+        });
     }
-    getBookNames(){
-        console.log(this.libros.map(el=>el.nombre));
+    getBookNames() {
+        let bookNames = [];
+        this.libros.forEach(libro => {
+            bookNames.push(libro.nombre);
+        })
+        return bookNames;
     }
 
 }
 
-
 let usuario = new Usuario("Elon", "Musk");
-usuario.getFullName();
+console.log(usuario.getFullName());
 usuario.addMascota("Perro");
 usuario.addMascota("Gato");
-usuario.countMascotas();
+console.log(usuario.countMascotas());
 usuario.addBook("El señor de las moscas", "William Golding");
 usuario.addBook("Fundacion", "Isaac Asimov");
-usuario.getBookNames();
-console.log(usuario);
+console.log(usuario.getBookNames());
