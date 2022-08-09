631981069eef74edcbfbc7f4ef754f29c3f1e4e6
fabianeromano <fabian.ed.romano@gmail.com>
2022-08-09 19:33:24 -0300

    Modificando el desafio 2 reentrega

--- index.js
+++ index.js
@@ -1,85 +1,91 @@
-const fs = require("fs");
-
-async function readFile(filename) {
-    try {
-        const contenido = await fs.promises.readFile(filename, 'utf-8')
-        return JSON.parse(contenido);
-    } catch (error) {
-        console.log('Hubo un error al leer el archivo: ' + filename);
-    }
-}
-
-async function writeFile(filename, contenido) {
-    try {
-        await fs.promises.writeFile(filename, JSON.stringify(contenido, null, 2));
-    } catch (error) {
-        console.log('Hubo un error al escribir el archivo: ' + filename);
-    }
-}
+const { promises: fs } = require("fs")
 
 class Contenedor {
-    constructor(filename) {
-        this.filename = filename
+    constructor(ruta) {
+        this.ruta = ruta
     }
-    async save({
-        title,
-        price,
-        thumbnail
-    }) {
-        // obtener todos los productos del archivo
-        const productos = await this.getAll();
-        const producto = {
-            id: productos.length + 1,
-            title,
-            price,
-            thumbnail
+
+
+    async save(nuevoObjeto) {
+        const objetos = await this.getAll()
+        let newId
+        if (objetos.length == 0) {
+            newId = 1
+        } else {
+            const ultimoId = parseInt(objetos[objetos.length - 1].id)
+            newId = ultimoId + 1;
         }
-        const newProductos = [...productos, producto];
-        writeFile(this.filename, newProductos);
+
+        objetos.push({ ...nuevoObjeto, id: newId })
+
+        try {
+            await fs.writeFile(this.ruta, JSON.stringify(objetos, null, 2))
+            return newId
+        } catch (error) {
+            throw new Error(`Error al guardar: ${error}`)
+        }
     }
     async getById(id) {
-        const productos = await readFile(this.filename);
-        return productos.find(prod => prod.id === id);
+        try {
+            const objetos = await this.getAll();
+            const filtrarObjetos = objetos.find((elem) => elem.id === id);
+            const response = filtrarObjetos === undefined ? null : filtrarObjetos;
+            return response;
+          } catch (error) {
+            throw new Error(`Error al buscar un producto por id: ${error}`);
+          }
     }
     async getAll() {
-        const productos = await readFile(this.filename);
-        return productos;
+        try {
+            const objetos = await fs.readFile(this.ruta, "utf-8")
+            return JSON.parse(objetos)
+        } catch (error) {
+            return []
+        }
     }
     async deleteById(id) {
-        const productos = await this.getAll();
-        await writeFile(this.filename, productos.filter(prod => prod.id !== id))
+        const objetos = await this.getAll();
+        const nuevoDato = objetos.filter((elem) => elem.id !== id);
+        if (nuevoDato.length === objetos.length) {
+          throw new Error(`Error while deleting. The id: ${id} was not found.`);
+        }
+        try {
+            await fs.writeFile(this.ruta, JSON.stringify(objetos, null, 2))
+        } catch (error) {
+          throw new Error(`Error while deleting.`);
+        }
     }
     async deleteAll() {
-        await writeFile(this.filename, [])
+        try {
+            const content = [];
+            await fs.writeFile(this.ruta, JSON.stringify(content, null, 2))
+        } catch (error) {
+            throw new Error(error);
+        }
     }
 }
 
-async function main() {
-    const c = new Contenedor('./productos.txt');
-    c.deleteAll();
-    // Agregar un producto al archivo
-    await c.save({
-        title: 'Escuadra',
-        price: 123.45,
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
-    });
-    await c.save({
-        title: 'Calculadora',
-        price: 234.56,
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',
-        id: 2
-    })
-    await c.save({
-        title: 'Globo Terráqueo',
-        price: 345.67,
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',
-        id: 3
-    })
-    console.log('TODOS LOS PRODUCTOS')
-    console.log(await c.getAll());
-    console.log('PRODUCTO CON ID 3')
-    console.log(await c.getById(3));
-    setTimeout(() => c.deleteById(2), 2000);
-}
+(async function mostrarResultados(){
+    const listaProductos = new Contenedor(`./productos.txt`)
 
-main()
+	await listaProductos.save({
+		title: 'Escuadra',                                                                                                                                 
+        price: 123.45,                                                                                                                                     
+        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png' 
+	});
+	await listaProductos.save({
+		title: 'Calculadora',                                                                                                                              
+        price: 234.56,                                                                                                                                     
+        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'  
+	});
+    await listaProductos.save({
+		title: 'Globo Terráqueo',                                                                                                                          
+        price: 345.67,                                                                                                                                     
+        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'    
+	});
+
+	console.log(await listaProductos.getById(1));
+	await listaProductos.getAll();
+	await listaProductos.deleteById(1);
+	await listaProductos.deleteAll();
+})();
